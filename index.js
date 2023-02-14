80f528e42fcc3f5f2b57e0f206ebe0f15e208c29
VivekJawale <vivekjawale41@gmail.com>
2023-02-14 11:14:07 +0530

    added

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server"); 
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; 
+
+app.use(middlewares);
+app.use(router);
+
+app.listen(port);
